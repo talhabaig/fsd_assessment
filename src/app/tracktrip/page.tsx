7d184f7966874b08588c3b4d723f5eb9bee968ca
talha <baigtalha7@gmail.com>
2024-07-03 12:24:30 +0500

    track trip hero

--- src/app/tracktrip/page.tsx
+++ src/app/tracktrip/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "./Hero";
+import Content from "./Content";
 
 export default function page() {
   return (
-    <div className='min-h-screen'>
-      Track Trip page
+    <div className="min-h-[100vh] min-w-[100%]">
+      <Hero />
+      <Content />
     </div>
-  )
+  );
 }
