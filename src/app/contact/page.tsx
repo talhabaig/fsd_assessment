0bda25581827de6216eeb505de1a60a06ae7c889
talha <baigtalha7@gmail.com>
2024-07-05 14:19:56 +0500

    add back button to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,12 +8,20 @@ export default function Page() {
   const [submitted, setSubmitted] = useState(false);
   return (
     <>
-      {submitted ? (
+      {!submitted ? (
         <div className="flex justify-center items-center min-h-[100vh]">
-          <span className="text-reddishbrown text-[20px] font-semibold leading-7">
-            Thank you for connecting with us <br /> Our Team Member Will Contact
-            You Soon !
-          </span>
+          <div className="flex gap-5">
+            <span className="text-reddishbrown text-[20px] font-semibold leading-7">
+              Thank you for connecting with us <br /> Our Team Member Will
+              Contact You Soon !
+            </span>
+            <button
+              onClick={() => setSubmitted(false)}
+              className="mt-4 px-[22px] py-[10px]  text-[16px] font-bold leading-normal text-white bg-oceanBlue border-2 self-stretch"
+            >
+              Go Back
+            </button>
+          </div>
         </div>
       ) : (
         <div className="min-h-[100vh] min-w-[100%] flex flex-col gap-[25rem]">
